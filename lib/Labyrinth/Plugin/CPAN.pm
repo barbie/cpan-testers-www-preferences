6d47e70e3fa73d67e7498ba601647a2aba1adfec
Barbie <barbie@missbarbell.co.uk>
2014-07-05 09:21:57 +0100

    renamed distribution, to use full name

--- lib/Labyrinth/Plugin/CPAN.pm
+++ lib/Labyrinth/Plugin/CPAN.pm
@@ -3,7 +3,7 @@ package Labyrinth::Plugin::CPAN;
 use strict;
 use warnings;
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 =head1 NAME
 
--- lib/Labyrinth/Plugin/CPAN/Preferences.pm
+++ lib/Labyrinth/Plugin/CPAN/Preferences.pm
@@ -3,7 +3,7 @@ package Labyrinth::Plugin::CPAN::Preferences;
 use strict;
 use warnings;
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 =head1 NAME
 
